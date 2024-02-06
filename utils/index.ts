3492f5b8af96f9cf0405690e5d6e3cb25186c226
Ackarmen <lyaforis@gmail.com>
2024-02-06 17:45:00 +0300

    fix bug with apiKeys

--- utils/index.ts
+++ utils/index.ts
@@ -1,11 +1,10 @@
-import { rapidApiKey } from "@/constants";
 import { CarProps, FilterProps } from "@/types";
 
 export async function fetchCars(filters: FilterProps) {
   const { manufacturer, year, model, limit, fuel } = filters;
 
-  const headers = {
-    "X-RapidAPI-Key": `${rapidApiKey}`,
+  const headers: HeadersInit = {
+    "X-RapidAPI-Key": process.env.NEXT_PUBLIC_RAPID_API_KEY || "",
     "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
   };
 
@@ -39,7 +38,10 @@ export const generateCarImageUrl = (car: CarProps, angle?: string) => {
 
   const { make, year, model } = car;
 
-  url.searchParams.append("customer", "hrjavascript-mastery");
+  url.searchParams.append(
+    "customer",
+    process.env.NEXT_PUBLIC_IMAGIN_API_KEY || ""
+  );
   url.searchParams.append("make", make);
   url.searchParams.append("modelFamily", model.split(" ")[0]);
   url.searchParams.append("zoomType", `${year}`);
