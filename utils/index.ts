c0e1dcebf706f78ecea19ffeb915d676c83ae506
Ackarmen <lyaforis@gmail.com>
2024-02-06 22:54:02 +0300

    commit

--- utils/index.ts
+++ utils/index.ts
@@ -4,7 +4,7 @@ export async function fetchCars(filters: FilterProps) {
   const { manufacturer, year, model, limit, fuel } = filters;
 
   const headers: HeadersInit = {
-    "X-RapidAPI-Key": process.env.RAPID_API_KEY || "",
+    "X-RapidAPI-Key": `${process.env.RAPID_API_KEY}`,
     "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
   };
 
@@ -38,7 +38,7 @@ export const generateCarImageUrl = (car: CarProps, angle?: string) => {
 
   const { make, year, model } = car;
 
-  url.searchParams.append("customer", process.env.IMAGIN_API_KEY || "");
+  url.searchParams.append("customer", `${process.env.IMAGIN_API_KEY}`);
   url.searchParams.append("make", make);
   url.searchParams.append("modelFamily", model.split(" ")[0]);
   url.searchParams.append("zoomType", "fullscreen");
